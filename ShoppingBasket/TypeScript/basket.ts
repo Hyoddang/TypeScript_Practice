4fd1fd8b719883fd3eaf9896e2c80f9a90ec2847
Hyoddang <yhd5689@naver.com>
2024-04-07 19:59:51 +0900

    add Cart 버튼에 기능 추가, 로컬 스토리지 업뎃 메서드 작성

--- ShoppingBasket/TypeScript/basket.ts
+++ ShoppingBasket/TypeScript/basket.ts
@@ -1,4 +1,86 @@
 interface Basket {
-  itemSum: number;
+  itemNum: number;
 }
 
+//? 버튼 클릭 시 hidden 태그 on 메서드
+//? 버튼 클릭 이벤트 설정 ( 누를 때마다 +1 ) O
+//? 9개가 넘어가면 9+로 표시
+
+class addEvent {
+  private static instance: addEvent | null = null;
+
+  public static getInstance(): addEvent {
+    if (!this.instance) {
+      this.instance = new addEvent();
+    }
+    return this.instance;
+  }
+
+  public addButton(): void {
+    const addButtonClick: HTMLElement | null = document.querySelector(".add-item-btn");
+
+    if (addButtonClick) {
+      addButtonClick.onclick = () => {
+        addService.getInstance().addBtnClick();
+      }
+    }
+  }
+
+  public hiddenRemove(): void {
+    const removeHidden: HTMLElement | null = document.querySelector(".sum-container")
+
+    if (removeHidden) {
+      
+    }
+  }
+}
+
+
+class addService {
+  private static instance: addService | null = null;
+
+  public basketList: Basket[];
+
+  public static getInstance(): addService {
+    if (!this.instance) {
+      this.instance = new addService();
+    }
+    return this.instance;
+  }
+  
+  constructor() {
+    const storedShopping: string | null = localStorage.getItem("basketList");
+
+    this.basketList = storedShopping ? JSON.parse(storedShopping) : [];
+
+    this.loadShoppingList();
+  }
+
+  public addBtnClick(): void {
+    const addBtn: HTMLElement | null = document.querySelector(".add-item-btn");
+
+    const basket: Basket = {
+      itemNum: this.basketList.length
+    }
+    
+    if (addBtn) {
+      addBtn.onclick = () => {
+        this.basketList.push(basket)
+      }
+    }
+  }
+
+  public uploadShoppingList(): void {
+    localStorage.setItem("basketList", JSON.stringify(this.basketList))
+
+    this.loadShoppingList();
+  }
+
+  public loadShoppingList(): void {
+    const loadList: HTMLElement | null = document.querySelector(".sum-item")
+
+    if (!loadList) return ;
+
+    loadList.innerHTML += `${this.basketList.length + 1}`
+  }
+}
