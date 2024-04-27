0489ad8c025239fbfad6f07c43aa01a1d6725f63
Hyoddang <yhd5689@naver.com>
2024-04-27 20:27:21 +0800

    Sequential Typing Effect 수정 필요

--- FrontEnd_Animation/Typing_Effect/JavaScript/typingEffect.js
+++ FrontEnd_Animation/Typing_Effect/JavaScript/typingEffect.js
@@ -2,33 +2,46 @@ let textEffect = document.querySelector('.text-effect');
 
 let stringArr = ["안녕하세요.", "프론트엔드 개발자를 꿈 꾸는", "Hyo 입니다."];
 
-let selectStringArr = selectString.split("");
-
 let currentIndex = 0;
 
-function nextStr() {
+let selectString = stringArr[currentIndex];
 
+let selectStringArr = selectString.split("");
+
+function nextStr() {
+  let stringArr = ["안녕하세요.", "프론트엔드 개발자를 꿈 꾸는", "Hyo 입니다."];
+
+  let currentIndex = 0;
+
+  let selectString = stringArr[currentIndex];
+
+  let selectStringArr = selectString.split("");
+
+  if (currentIndex === stringArr.length) {
+    currentIndex = 0;
+  }
+  return selectStringArr;
 }
 
 // 타이핑효과 리셋
 function resetTyping() {
   textEffect.textContent = '';
-  strEffect(randomString());
+  strEffect(nextStr());
 }
 
 // 한 글자씩 텍스트 출력하는 함수
-function strEffect(randomArr) {
-  if(randomArr.length > 0) {
-    textEffect.textContent += randomArr.shift();
+function strEffect(strArr) {
+  if(strArr.length > 0) {
+    textEffect.textContent += strArr.shift();
     setTimeout(function() {
-      strEffect(randomArr);
+      strEffect(strArr);
     }, 80);
   } else {
     setTimeout(resetTyping, 1500);
   }
 }
 
-strEffect(randomString());
+strEffect(nextStr());
 
 // 커서 깜빡임 효과
 function blink() {
